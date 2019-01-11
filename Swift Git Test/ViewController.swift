3368cf171af85663978242875ec8cd45e1b720bb
Ben Norman <Ben@users-macbook-air-2.local>
2019-01-11 15:12:05 +0000

    Initial Commit

---
+++ Swift Git Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Git Test
+//
+//  Created by Ben Norman on 11/01/2019.
+//  Copyright © 2019 Ben Norman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
