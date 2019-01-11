c8b19da0b02286e3b89406966cb78f491b91d758
Ben Norman <Ben@users-macbook-air-2.local>
2019-01-11 15:49:24 +0000

    number bingos

--- Swift Git Test/ViewController.swift
+++ Swift Git Test/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    var number = 0
+    var numbers = 0
     
 }
 
